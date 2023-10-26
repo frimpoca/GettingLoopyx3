8fe4528e6e700a71e57c07f6d44e090535068223
frimpoca <frimpoca@mail.uc.edu>
2023-10-26 18:37:44 -0400

    x3

---
+++ src/Loop3.java
@@ -0,0 +1,8 @@
+public class Loop3 { // Start of class
+    public static void main(String [] args){ // start of main()
+        for (int i=0; i<=18; i=i+3){ // watch out for i=i+3, as the increment is by 3
+            System.out.println(i); // print i
+        } // end of for loop
+    } // end of main()
+} // end of class
+
